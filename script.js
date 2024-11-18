b6d709d966ae895c4e4d11762f767b57eb7c0375
nymishkash <reachnymish@gmail.com>
2024-11-18 23:11:13 +0530

    prettier

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Get the button
-const darkModeBtn = document.getElementById('darkModeBtn');
+const darkModeBtn = document.getElementById("darkModeBtn");
 
 // Add click event
-darkModeBtn.addEventListener('click', function() {
-    // Toggle dark mode class on body
-    document.body.classList.toggle('dark-mode');
-    
-    // Change button text
-    if(document.body.classList.contains('dark-mode')) {
-        darkModeBtn.textContent = 'Light Mode';
-    } else {
-        darkModeBtn.textContent = 'Dark Mode';
-    }
+darkModeBtn.addEventListener("click", function () {
+  // Toggle dark mode class on body
+  document.body.classList.toggle("dark-mode");
+
+  // Change button text
+  if (document.body.classList.contains("dark-mode")) {
+    darkModeBtn.textContent = "Light Mode";
+  } else {
+    darkModeBtn.textContent = "Dark Mode";
+  }
 });
